e85c7d32ec2ae199f7fd0eb7dc5197069c23654a
Matt Harasymczuk <matt@astrotech.io>
2019-05-28 09:02:19 +0200

    Person

--- contact/models/person.py
+++ contact/models/person.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from typing import Optional
-from pathlib import Path
 from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
